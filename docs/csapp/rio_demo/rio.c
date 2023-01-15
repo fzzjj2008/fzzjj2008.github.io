34d25ecf8a92e0cb7c2011423d07ae82d28c2862
fzzjj2008 <fzzjj2008@126.com>
2023-01-15 20:07:52 +0800

    add csapp

--- docs/csapp/rio_demo/rio.c
+++ docs/csapp/rio_demo/rio.c
@@ -9,21 +9,21 @@
 
 ssize_t rio_readn(int fd, void *usrbuf, size_t n) 
 {
-    size_t nleft = n; //剩下未读字符数
+    size_t nleft = n;           //剩下未读字符数
     ssize_t nread;
     char *bufp = usrbuf;
 
     while (nleft > 0) {
-	if ((nread = read(fd, bufp, nleft)) < 0) {
-        if (errno == EINTR)  //被信号处理函数中断
-		nread = 0;      //本次读到0个字符，再次读取
-	    else
-		return -1;      //出错，errno由read设置
-    } 
-	else if (nread == 0) //读取到EOF
-        break;              
-    nleft -= nread; //剩下的字符数减去本次读到的字符数
-	bufp += nread;  //缓冲区指针向右移动
+        if ((nread = read(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) //被信号处理函数中断
+                nread = 0;      //本次读到0个字符，再次读取
+            else
+                return -1;      //出错，errno由read设置
+        } 
+        else if (nread == 0)    //读取到EOF
+            break;              
+        nleft -= nread;         //剩下的字符数减去本次读到的字符数
+        bufp += nread;          //缓冲区指针向右移动
     }
     //返回实际读取的字符数
     return (n - nleft);         /* return >= 0 */
@@ -36,14 +36,14 @@ ssize_t rio_writen(int fd, void *usrbuf, size_t n)
     char *bufp = usrbuf;
 
     while (nleft > 0) {
-	if ((nwritten = write(fd, bufp, nleft)) <= 0) {
-	    if (errno == EINTR)
-		nwritten = 0;
-	    else
-		return -1;
-	}
-	nleft -= nwritten;
-	bufp += nwritten;
+        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR)
+                nwritten = 0;
+            else
+                return -1;
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
     }
     return n;
 }
@@ -60,22 +60,21 @@ static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
     int cnt;
 
     while (rp->rio_cnt <= 0) {  //缓冲区为空，调用read填充
-	rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
-			   sizeof(rp->rio_buf));
-	if (rp->rio_cnt < 0) {
-	    if (errno != EINTR) /* Interrupted by sig handler return */
-		return -1;
-	}
-	else if (rp->rio_cnt == 0)  /* EOF */
-	    return 0;
-	else
-	    rp->rio_bufptr = rp->rio_buf; /* Reset buffer ptr */
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, sizeof(rp->rio_buf));
+        if (rp->rio_cnt < 0) {
+            if (errno != EINTR) /* Interrupted by sig handler return */
+                return -1;
+        }
+        else if (rp->rio_cnt == 0)  /* EOF */
+            return 0;
+        else
+            rp->rio_bufptr = rp->rio_buf; /* Reset buffer ptr */
     }
 
     /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
     cnt = n;
     if (rp->rio_cnt < n)
-	cnt = rp->rio_cnt;
+        cnt = rp->rio_cnt;
     memcpy(usrbuf, rp->rio_bufptr, cnt);
     rp->rio_bufptr += cnt;
     rp->rio_cnt -= cnt;
@@ -89,12 +88,12 @@ ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
     char *bufp = usrbuf;
 
     while (nleft > 0) {
-	if ((nread = rio_read(rp, bufp, nleft)) < 0)
+        if ((nread = rio_read(rp, bufp, nleft)) < 0)
             return -1;          /* errno set by read() */
-	else if (nread == 0)
-	    break;              /* EOF */
-	nleft -= nread;
-	bufp += nread;
+        else if (nread == 0)
+            break;              /* EOF */
+        nleft -= nread;
+        bufp += nread;
     }
     return (n - nleft);         /* return >= 0 */
 }
@@ -106,18 +105,18 @@ ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
 
     for (n = 1; n < maxlen; n++) {
         if ((rc = rio_read(rp, &c, 1)) == 1) {
-	    *bufp++ = c;
-	    if (c == '\n') {
+            *bufp++ = c;
+            if (c == '\n') {
                 n++;
-     		break;
+                break;
             }
-	} else if (rc == 0) {
-	    if (n == 1)
-		return 0; //第一次读取就到了EOF
-	    else
-		break;    //读了一些数据后遇到EOF
-	} else
-	    return -1;	  /* Error */
+        } else if (rc == 0) {
+            if (n == 1)
+                return 0; //第一次读取就到了EOF
+            else
+                break;    //读了一些数据后遇到EOF
+        } else
+            return -1;    /* Error */
     }
     *bufp = 0;
     return n-1;
